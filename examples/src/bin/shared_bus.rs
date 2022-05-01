b6fbd7502b6c2cba20109750a247e7f01efc312f
Henrik Alsér <henrik.alser@me.com>
2022-05-01 09:13:41 +0200

    Shared i2c bus example

--- examples/src/bin/shared_bus.rs
+++ examples/src/bin/shared_bus.rs
@@ -12,10 +12,11 @@ use embassy::executor::Spawner;
 use embassy::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy::mutex::Mutex;
 use embassy::util::Forever;
-use embassy::time::{Duration, Timer};
+use embassy::time::{Delay, Duration, Timer};
 use embassy_nrf::twim::{self, Twim};
 use embassy_nrf::{interrupt, Peripherals, peripherals::TWISPI0};
 use nrf_embassy::shared_i2c::I2cBusDevice;
+use mpu6050_async::*;
 use qmc5883l_async::*;
 use core::f32::consts::PI;
 use libm::atan2;
@@ -24,18 +25,9 @@ use libm::atan2;
 // readings. See http://www.magnetic-declination.com/
 const DECLINATION_RADS: f32 = 0.024434609;
 
-#[embassy::main]
-async fn main(_spawner: Spawner, p: Peripherals) {
-    static I2C_BUS: Forever<Mutex::<ThreadModeRawMutex, Twim<TWISPI0>>> = Forever::new();
-    let config = twim::Config::default();
-    let irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
-    let i2c_bus = Mutex::<ThreadModeRawMutex, Twim<TWISPI0>>::new(Twim::new(p.TWISPI0, irq, p.P0_03, p.P0_04, config));
-    let i2c_bus = I2C_BUS.put(i2c_bus);
-    let i2c_dev = I2cBusDevice::new(i2c_bus);
-
-    let mut compass = QMC5883L::new(i2c_dev).await.unwrap();
+#[embassy::task]
+async fn compass_task(mut compass: QMC5883L<I2cBusDevice<'static, ThreadModeRawMutex, Twim<'_, TWISPI0>>>) {
     compass.continuous().await.unwrap();
-
     loop {
         if let Ok((x, y, z)) = compass.mag().await {
             let mut heading = atan2(y as f64, x as f64) as f32 + DECLINATION_RADS;
@@ -49,7 +41,35 @@ async fn main(_spawner: Spawner, p: Peripherals) {
                 "x={}, y={}, z={}: heading={} degrees",
                 x, y, z, heading_degrees
             );
-            Timer::after(Duration::from_millis(500)).await;
+            Timer::after(Duration::from_millis(1000)).await;
         }
     }
+}
+
+#[embassy::task]
+async fn gyro_task(mut mpu: Mpu6050<I2cBusDevice<'static, ThreadModeRawMutex, Twim<'_, TWISPI0>>>) {
+    mpu.init(&mut Delay).await.unwrap();
+    loop {
+        // Get gyro data, scaled with sensitivity
+        let gyro = mpu.get_gyro().await.unwrap();
+        info!("gyro: {:?}", (gyro[0], gyro[1], gyro[2]));
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
+
+#[embassy::main]
+async fn main(spawner: Spawner, p: Peripherals) {
+    static I2C_BUS: Forever<Mutex::<ThreadModeRawMutex, Twim<TWISPI0>>> = Forever::new();
+    let config = twim::Config::default();
+    let irq = interrupt::take!(SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0);
+    let i2c_bus = Mutex::<ThreadModeRawMutex, Twim<TWISPI0>>::new(Twim::new(p.TWISPI0, irq, p.P0_03, p.P0_04, config));
+    let i2c_bus = I2C_BUS.put(i2c_bus);
+
+    let i2c_dev1 = I2cBusDevice::new(i2c_bus);
+    let compass = QMC5883L::new(i2c_dev1).await.unwrap();
+    unwrap!(spawner.spawn(compass_task(compass)));
+    
+    let i2c_dev2 = I2cBusDevice::new(i2c_bus);
+    let mpu = Mpu6050::new(i2c_dev2); 
+    unwrap!(spawner.spawn(gyro_task(mpu)));
 }
